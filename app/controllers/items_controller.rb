921f98122ca0e3d855bff9bbccc7211e34a64eee
fujicho <nnnn.sohei.fujita@gmail.com>
2020-01-02 22:07:39 +0900

    インスタンス名の変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   def index
-    @item = Item.includes(:images).order("created_at DESC").limit(10)
+    @new_items = Item.includes(:images).order("created_at DESC").limit(10)
   end
 
 end
