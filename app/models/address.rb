807d82b42c2516715d4c048b16ed3267ec7be101
matsu1005 <forte4262@gmail.com>
2020-01-06 22:26:17 +0900

    addressモデルのvalidation

---
+++ app/models/address.rb
@@ -0,0 +1,16 @@
+class Address < ApplicationRecord
+
+  validates :post_first_name,      presence: true, length: { maximum: 30 },format: { with: /\A[ぁ-んァ-ヶー一-龠]+\z/ }
+  validates :post_last_name,       presence: true, length: { maximum: 30 },format: { with: /\A[ぁ-んァ-ヶー一-龠]+\z/ }
+  validates :post_first_name_kana, presence: true, length: { maximum: 30 },format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/ }
+  validates :post_last_name_kana,  presence: true, length: { maximum: 30 },format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/ }
+  validates :post_number,          presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}+\z/ }
+  validates :prefecture,           presence: true
+  validates :city,                 presence: true
+  validates :address_line,         presence: true
+  validates :building,             presence: false
+  validates :phone_number,         presence: true, format: { with: /\A[0-9]{11}+\z/ }
+  
+  belongs_to :user
+
+end
