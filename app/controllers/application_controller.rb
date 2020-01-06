74d56fa7c2f8fb830afaac22ab07caf2367dbeff
matsu1005 <forte4262@gmail.com>
2020-01-06 22:27:19 +0900

    before_acitonサインアップへ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :sign_up_action
+
+  def sign_up_action
+    unless user_signed_in?
+    @user = User.new
+    render template: 'signup/step1'
+    end
+  end
 end
