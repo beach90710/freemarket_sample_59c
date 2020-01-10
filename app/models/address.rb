7aa22a9f63337f9d3ebc7492f3e3ad3eebbf0821
matsu1005 <forte4262@gmail.com>
2020-01-10 19:38:46 +0900

    バリデーションの修正

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,7 +4,7 @@ class Address < ApplicationRecord
   validates :post_last_name,       presence: true, length: { maximum: 30 },format: { with: /\A[ぁ-んァ-ヶー一-龠]+\z/ }
   validates :post_first_name_kana, presence: true, length: { maximum: 30 },format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/ }
   validates :post_last_name_kana,  presence: true, length: { maximum: 30 },format: { with: /\A[ア-ン゛゜ァ-ォャ-ョー「」、]+\z/ }
-  validates :post_number,          presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}+\z/ }
+  validates :post_number,          presence: true, format: { with: /\A[0-9]{7}+\z/ }
   validates :prefecture,           presence: true
   validates :city,                 presence: true
   validates :address_line,         presence: true
