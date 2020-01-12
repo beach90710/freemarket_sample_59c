f4b112d72fe866ae3a5ea165a86669f01c135c70
fujicho <nnnn.sohei.fujita@gmail.com>
2020-01-12 15:23:43 +0900

    商品登録機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,21 @@ class ItemsController < ApplicationController
   def index
     @new_items = Item.includes(:images).order("created_at DESC").limit(10)
   end
+  
+  def new
+    @item = Item.new
+    3.times{ @item.images.build }
+  end
+  
+  def create
+    Item.create!(item_params)
+    # binding.pry
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name,:conditon,:detail,:price,:shipping_charge_fee,:shipping_method,:shipping_origin,:days_to_shipping,:brand_id,:category_id,:user_id,:image)
+  end
 
 end
