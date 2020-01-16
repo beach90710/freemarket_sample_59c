bf093a748fbe5153e2ee6cb1163da197e1fa4107
fujicho <nnnn.sohei.fujita@gmail.com>
2020-01-16 22:15:57 +0900

    editビューの追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     get "sign_out", to: "users/sessions#destroy" 
   end
 
-  resources :items,only: [:index,:new,:create]
+  resources :items,only: [:index,:new,:create,:edit]
   
   resources :test,only: [:create,:index,:new]
 end
