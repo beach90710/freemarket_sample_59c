574daa211ac6f6fc858b2bd26612483c8539f6f8
fujicho <nnnn.sohei.fujita@gmail.com>
2020-01-18 12:01:42 +0900

    status追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,11 +14,20 @@ class ItemsController < ApplicationController
     # binding.pry
     Item.create!(item_params)
   end
-  
+
+  def edit
+    @edit_item = Item.find(params[:id])
+  end
+
+  def update
+    edit_item = Item.find(params[:id])
+    edit_item.update(item_params)
+  end
+
   private
   
   def item_params
-    params.require(:item).permit(:id,:name,:conditon,:detail,:price,:shipping_charge_fee,:shipping_method,:shipping_origin,:days_to_shipping,:brand_id,:category_id,:user_id,images_attributes:[:image,:id])
+    params.require(:item).permit(:id,:name,:conditon,:detail,:price,:shipping_charge_fee,:shipping_method,:shipping_origin,:days_to_shipping,:brand_id,:category_id,:user_id,:status,images_attributes:[:image,:id])
   end
 
 end
