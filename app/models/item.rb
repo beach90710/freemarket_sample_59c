04757750621cabddd4a8c4564094096316ff14f2
fujicho <nnnn.sohei.fujita@gmail.com>
2020-02-02 17:28:41 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,13 @@ class Item < ApplicationRecord
   belongs_to :user
   belongs_to :brand,optional: true
   belongs_to :category,optional: true
+
+  validates :conditon,           presence: true
+  validates :detail,              presence: true,length: { maximum: 40 }
+  validates :price,               presence: true,numericality: { only_integer: true,greater_than_or_equal_to: 300,less_than_or_equal_to:  9999999}
+  validates :name,                presence: true,length: { maximum: 1000 }
+  validates :shipping_method,     presence: true
+  validates :shipping_origin,     presence: true
+  validates :days_to_shipping,    presence: true
+  validates :category_id,         presence: true
 end
