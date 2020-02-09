bb54fff0b0a12dcd07091d7d8c350678279228a3
matsu1005 <forte4262@gmail.com>
2020-02-09 09:29:01 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :sign_up_action
+  before_action :sign_up_action, except: [:index]
 
   def sign_up_action
     unless user_signed_in?
