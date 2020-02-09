5210b30f92958eb928782f2562dacf639b2ca723
ida4594 <kaha4594@yahoo.co.jp>
2020-02-09 20:33:33 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,6 +34,7 @@ class ItemsController < ApplicationController
   def destroy
     item = Item.find(params[:id])
       if item.user_id == current_user.id && item.destroy
+        redirect_to items_path notice:'削除できました'
       else
         redirect_to items_path notice:'削除できませんでした'
       end
