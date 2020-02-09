5327b9b4ffbfafa3471d4d332bed6660d5965aac
matsu1005 <forte4262@gmail.com>
2020-02-09 18:38:23 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :sign_up_action
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :sign_up_action, except: [:index, :show]
 
   private
 
@@ -22,4 +22,6 @@ class ApplicationController < ActionController::Base
     render template: 'signup/step1'
     end
   end
+
+
 end
