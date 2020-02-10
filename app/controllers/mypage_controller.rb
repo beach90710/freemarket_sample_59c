bd15f6b4ac11f1598d1d4da39f58bf56f76da929
matsu1005 <forte4262@gmail.com>
2020-02-10 21:57:43 +0900

    Update mypage_controller.rb

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -2,32 +2,6 @@ class MypageController < ApplicationController
   
   before_action :set_user
 
-
-  def index
-   
-  end
-
-  def profile
-  end
-
-  def deliver_address
-  end
-
-  def card
-  end
-
-  def email_password
-  end
-
-  def identification
-  end
-  
-  def sms_confirmation
-  end
-
-  def logout
-  end
-
   def set_user
     @user = current_user
   end
